375efa65b99888c89a47c8cc3233b9499d92621f
Заид Мингалиев <zzmingaliev@outlook.com>
2021-12-30 23:00:44 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using LR6_PP.Util;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace LR6_PP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public HtmlResult Func(int input)
+        {
+            return new HtmlResult(Convert.ToString(input * input));
+        }
+    }
+}
---
+++ Util/HtmlResult.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LR6_PP.Util
+{
+    public class HtmlResult : IActionResult
+    {
+        string htmlCode;
+        public HtmlResult(string html)
+        {
+            htmlCode = html;
+        }
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            string fullHtmlCode = "<!DOCTYPE html><html><head>";
+            fullHtmlCode += "<title>Главная страница</title>";
+            fullHtmlCode += "<meta charset=utf-8 />";
+            fullHtmlCode += "</head> <body>";
+            fullHtmlCode += "<div>" + htmlCode + "</div>";
+            fullHtmlCode += "</body></html>";
+            await context.HttpContext.Response.WriteAsync(fullHtmlCode);
+        }
+    }
+}
